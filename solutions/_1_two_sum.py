e0ae03f0b1fedf1702c7ac4d6ea1af6e16535fe7
go-go-megaman <46243301+go-go-megaman@users.noreply.github.com>
2019-08-25 20:15:41 +0900

    refactor: Refactor to traverse the list only once
    On account of traverse the list twice.

--- solutions/_1_two_sum.py
+++ solutions/_1_two_sum.py
@@ -1,11 +1,11 @@
 class Solution:
     def main(self, numbers: [int], target: int) -> [int]:
-        numbers_dict = {number: True for number in numbers}
-
+        numbers_dict = {}
         for number in numbers:
             paired_number = target - number
             exists = paired_number in numbers_dict.keys()
             if exists:
-                return [number, paired_number]
+                return [paired_number, number]
+            numbers_dict[number] = True
 
         raise ValueError("There is no pair their sum equals to target.")
